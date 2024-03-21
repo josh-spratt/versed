95ed887aac6f1453155c3e8ae940c1941fa1cba5
Joshua Spratt <jspratt02@icloud.com>
2024-03-20 23:44:26 -0500

    update changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## 0.1.0 (2024-03-20)
+
+Initial Release
+
+### Features
+
+* Initialize a new config file for storing ESV API token.
+* Retrieve a verse and display its data in the terminal.
--- src/versed/__main__.py
+++ src/versed/__main__.py
@@ -26,29 +26,9 @@ def select_verse(scripture_reference: str):
         res = requests.get(url=url_str, headers=headers)
         print(res.text)
     else:
-        print("""No Versed configuration found. Please run python -m versed init and put your API token in the .versed
-        configuration file that is created""")
-    # curl -s -H $ESV_API_TOKEN 'https://api.esv.org/v3/passage/text/?q=Psalm+27:4' | jq '.passages[0]' -r
-    # sample payload:
-    # data = {
-    #     "query": "Psalm 27:4",
-    #     "canonical": "Psalm 27:4",
-    #     "parsed": [[19027004, 19027004]],
-    #     "passage_meta": [
-    #         {
-    #             "canonical": "Psalm 27:4",
-    #             "chapter_start": [19027001, 19027014],
-    #             "chapter_end": [19027001, 19027014],
-    #             "prev_verse": 19027003,
-    #             "next_verse": 19027005,
-    #             "prev_chapter": [19026001, 19026012],
-    #             "next_chapter": [19028001, 19028009],
-    #         }
-    #     ],
-    #     "passages": [
-    #         "Psalm 27:4\n\n    [4] One thing have I asked of the LORD,\n        that will I seek after:\n    that I may dwell in the house of the LORD\n        all the days of my life,\n    to gaze upon the beauty of the LORD\n        and to inquire(1) in his temple.\n    \n\nFootnotes\n\n(1) 27:4 Or *meditate*\n (ESV)"
-    #     ],
-    # }
+        print(
+            "No Versed configuration found. Please run python -m versed init and put your API token in the .versed configuration file that is created"
+        )
 
 
 if __name__ == "__main__":
